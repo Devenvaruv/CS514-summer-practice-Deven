e0fe040afeef3f3b40e6c7c9ba88c19ecb6c75f6
your_github_username <devenvaruv@gmail.com>
2023-06-23 21:42:16 -0700

    d

--- src/main/ConversionsHelper.java
+++ src/main/ConversionsHelper.java
@@ -1,34 +1,14 @@
 package main;
 
-/**
- * A class to help with several common conversions.
- * @author srollins
- *
- */
 public class ConversionsHelper {
 
-    /**
-     * Converts ounces to gallons.
-     * One gallon is 128 ounces.
-     *
-     * @param ounces
-     * @return
-     */
     public static double ouncesToGallons(int ounces) {
-        //TODO: Replace with your code.
-        return 0;
+        return Math.round(ounces/128d * 10000d) / 10000d;
     }
 
-    /**
-     * Converts centimeters to feet.
-     * One foot is 30.48 cm.
-     *
-     * @param cm
-     * @return
-     */
+
     public static double cmToFeet(int cm) {
-        //TODO: Replace with your code.
-        return 0;
+        return cm /30.48d;
     }
 
     public static void main(String[] args) {
